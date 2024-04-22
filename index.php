099b30c06de4419404cf2136cbcaede0d91a2f7a
unknown <favoriteinfinite@gmail.com>
2024-04-22 17:07:38 +0800

    初始架構

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AniMate online shop</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="./js/jquery-1.9.1min.js"></script>
+    <script src="./js/js.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+</head>
+
+<body>
+
+    <header>
+        
+    </header>
+
+
+    <main>
+
+    </main>
+
+
+    <footer>
+
+    </footer>
+
+</body>
+
+</html>
