0735a05bc6fd47948b676289c3d7209ad10beafb
unknown <favoriteinfinite@gmail.com>
2024-04-26 00:02:20 +0800

    將首頁輪播圖連接資料庫

--- front/main.php
+++ front/main.php
@@ -1,28 +1,53 @@
 <style>
-  #carouselExampleIndicators{
+  #carouselExampleIndicators {
     width: 100%;
     height: 500px;
     overflow: hidden;
   }
 </style>
 <!-- carousel start -->
-<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+<?php
+// 順位第一的輪播圖「數字」
+$max_rank = $Carousel->max('rank');
+// 順位第一的輪播圖
+$first = $Carousel->all(" order by `rank` desc limit 1");
+// 輪播圖數量
+$all_car = $Carousel->count(['sh' => 1]);
+// 第一張之後的所有輪播圖
+$cars = $Carousel->q("select * from `carousel` where `rank`<$max_rank && `sh`=1 order by `rank` desc");
+?>
+<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel" style="height: auto;">
+  <!-- 輪播圖按鈕 -->
   <div class="carousel-indicators" style="gap: 3px;">
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1" style="width: 5px;height:5px;border-radius:50%"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2" style="width: 5px;height:5px;border-radius:50%"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3" style="width: 5px;height:5px;border-radius:50%"></button>
+    <?php
+    for ($i = 1; $i <= $all_car - 1; $i++) {
+    ?>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="<?= $i; ?>" aria-label="Slide <?= $i + 1; ?>" style="width: 5px;height:5px;border-radius:50%"></button>
+    <?php } ?>
   </div>
+  <!-- 輪播圖 -->
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="../img/bg1.jpg" class="d-block w-100" alt="...">
+      <?php
+      foreach ($first as $fir) {
+      ?>
+        <a href="<?= $fir['link']; ?>">
+          <img src="../img/<?= $fir['img']; ?>" class="d-block w-100" alt="...">
+        </a>
+      <?php } ?>
     </div>
-    <div class="carousel-item">
-      <img src="../img/bg2.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="../img/bg3.jpg" class="d-block w-100" alt="...">
-    </div>
+    <?php
+    foreach ($cars as $car) {
+    ?>
+      <div class="carousel-item">
+        <a href="<?= $car['link']; ?>">
+          <img src="../img/<?= $car['img']; ?>" class="d-block w-100" alt="...">
+        </a>
+      </div>
+    <?php } ?>
   </div>
+
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>
