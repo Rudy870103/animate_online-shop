57bbd1680f31c6e705edfc8ab11241f802b53e65
unknown <favoriteinfinite@gmail.com>
2024-04-25 18:14:07 +0800

    完成商品CRUD

--- back/product.php
+++ back/product.php
@@ -15,38 +15,40 @@
 
 
 <div style="width: 80%;margin:auto;">
-<div class="product">
-                <button onclick="location.href='?do=add_product'">新增商品</button>
-                <table>
-                    <tr>
-                        <td>編號</td>
-                        <td>商品圖片</td>
-                        <td>商品名稱</td>
-                        <td>庫存</td>
-                        <td>規格</td>
-                        <td>操作</td>
-                    </tr>
-                    <?php
-                    $rows = $Product->all(" order by `rank` desc");
-                    foreach ($rows as $idx => $row) {
-                    ?>
-                        <tr>
-                            <td><?= $row['no']; ?></td>
-                            <td><img src="../img/<?= $row['img']; ?>" style="width: 100px;"></td>
-                            <td><?= $row['name']; ?></td>
-                            <td><?= $row['stock']; ?></td>
-                            <td><?= $row['spec']; ?></td>
-                            <td>
-                                <button>修改</button>
-                                <button class="del-product" data-id="<?= $row['id']; ?>">刪除</button>
-                                <button class='show-btn login-btn' data-id="<?= $row['id']; ?>"><?= ($row['sh'] == 1) ? '下架' : '上架'; ?></button>
-                                <button>前移</button>
-                                <button>後移</button>
-                            </td>
-                        </tr>
-                    <?php } ?>
-                </table>
-            </div>
+    <div class="product">
+        <button onclick="location.href='?do=add_product'">新增商品</button>
+        <table>
+            <tr>
+                <td>商品編號</td>
+                <td>商品圖片</td>
+                <td>商品名稱</td>
+                <td>庫存</td>
+                <td>規格</td>
+                <td>操作</td>
+            </tr>
+            <?php
+            $rows = $Product->all(" order by `rank` desc");
+            foreach ($rows as $idx => $row) {
+            ?>
+                <tr>
+                    <td><?= $row['no']; ?></td>
+                    <td><img src="../img/<?= $row['img']; ?>" style="width: 100px;"></td>
+                    <td><?= $row['name']; ?></td>
+                    <td><?= $row['stock']; ?></td>
+                    <td><?= $row['spec']; ?></td>
+                    <td>
+                        <button onclick="location.href='?do=edit_product&id=<?=$row['id'];?>'">修改</button>
+                        <button class="del-product" data-id="<?= $row['id']; ?>">刪除</button>
+                        <button class='show-btn login-btn' data-id="<?= $row['id']; ?>"><?= ($row['sh'] == 1) ? '下架' : '上架'; ?></button>
+                        <button class='sw-btn login-btn' data-id="<?= $row['id']; ?>" data-sw="<?= ($idx !== 0) ? $rows[$idx - 1]['id'] : $row['id']; ?>">前移
+                        </button>
+                        <button class='sw-btn login-btn' data-id="<?= $row['id']; ?>" data-sw="<?= ((count($rows) - 1) != $idx) ? $rows[$idx + 1]['id'] : $row['id']; ?>">後移
+                        </button>
+                    </td>
+                </tr>
+            <?php } ?>
+        </table>
+    </div>
 </div>
 
 
@@ -62,18 +64,6 @@
         })
     }
 
-    
-
-    function sw(id, sh) {
-        $.post("./api/product_sw.php", {
-            id,
-            sh
-        }, () => {
-            location.reload();
-        })
-    }
-
-
     $(".del-product").on("click", function() {
         if (confirm("確定刪除該商品?")) {
             $.post("./api/del.php", {
@@ -88,7 +78,21 @@
     $(".show-btn").on("click", function() {
         let id = $(this).data('id');
         $.post("./api/show.php", {
-            table:'Product',id
+            table: 'Product',
+            id
+        }, () => {
+            location.reload()
+        })
+    })
+
+    $(".sw-btn").on("click", function() {
+        let id = $(this).data('id');
+        let sw = $(this).data('sw');
+        let table = 'product'
+        $.post("./api/sw.php", {
+            id,
+            sw,
+            table
         }, () => {
             location.reload()
         })
